7d7725b0b2a4ceaaf8d858dd989da35eb129d208
Annaliisa Tongpattanakul <annaliisa@Annaliisas-MacBook-Pro.local>
2020-01-16 11:57:25 -0800

    entries can be stored in local storage when save icon/button is clicked

--- planner.js
+++ planner.js
@@ -9,7 +9,7 @@ $('#currentDay').append(timeNow);
 let nineAm = $('#slot1').text();
 let tenAm = $('#slot2').text();
 let elevenAm = $('#slot3').text();
-let twelveAm = $('#slot4').text();
+let twelvePm = $('#slot4').text();
 let onePm = $('#slot5').text();
 let twoPm = $('#slot6').text();
 let threePm = $('#slot7').text();
@@ -35,7 +35,6 @@ function hourColor() {
     //   if has a value less than the current hour, sets color to grey
       if (hour < currentHour24) {
         $('#textInput' + i).css('background-color', 'lightgray');
-    
       } else {
         $('#textInput' + i ).css('background-color', 'lightgreen');
       }
@@ -43,5 +42,69 @@ function hourColor() {
   }
 }
 
-
+// on-click of save icon if there is a pre-exisitng value removes and replaces with existing value
+// currently in the text field in local storage so there is not a bunch of duplicate entries
+$('#btn1').click(function() {
+    // removes any existing items 
+    if($('#textInput1').val()) {
+      localStorage.removeItem('textInput1');
+    }
+    localStorage.setItem(nineAm, $('#textInput1').val());
+  });
+  
+  $('#btn2').click(function() {
+    if($('#textInput2').val()) {
+      localStorage.removeItem('textInput2');
+    }
+    localStorage.setItem(tenAm, $('#textInput2').val());
+  });
+  
+  $('#btn3').click(function() {
+    if($('#textInput3').val()) {
+      localStorage.removeItem('textInput3');
+    }
+    localStorage.setItem(elevenAm, $('#textInput3').val());
+  });
+  
+  $('#btn4').click(function() {
+    if($('#textInput4').val()) {
+      localStorage.removeItem('textInput4');
+    }
+    localStorage.setItem(twelvePm, $('#textInput4').val());
+  });
+  
+  $('#btn5').click(function() {
+    if($('#textInput5').val()) {
+      localStorage.removeItem('textInput5');
+    }
+    localStorage.setItem(onePm, $('#textInput5').val());
+  });
+  
+  $('#btn6').click(function() {
+    if($('#textInput6').val()) {
+      localStorage.removeItem('textInput6');
+    }
+    localStorage.setItem(twoPm, $('#textInput6').val());
+  });
+  
+  $('#btn7').click(function() {
+    if($('#textInput7').val()) {
+      localStorage.removeItem('textInput7');
+    }
+    localStorage.setItem(threePm, $('#textInput7').val());
+  })
+  
+  $('#btn8').click(function() {
+    if($('#textInput8').val()) {
+      localStorage.removeItem('textInput8');
+    }
+    localStorage.setItem(fourPm, $('#textInput8').val());
+  })
+  
+  $('#btn9').click(function() { 
+    if($('#textInput9').val()) {
+      localStorage.removeItem('textInput9');
+    }
+    localStorage.setItem(fivePm, $('#textInput9').val());
+  })
 
